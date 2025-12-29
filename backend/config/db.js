f96d597b15ce1c3530fcab4c1bff8662098ebb42
Shrimant Shaw <161811713+Shrimant05@users.noreply.github.com>
2025-12-29 19:57:04 +0530

    Admin panel complete

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://shrimant:Shrimant2005@cluster0.dtarrtf.mongodb.net/food-del').then(()=>{
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>{
         console.log('DB Connected')
     })
 }
